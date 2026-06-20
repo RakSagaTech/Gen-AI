130611ade7ce6a49d6f341ad62e0c117913d074b
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-06-20 10:44:11 +0530

    chore: configure cookie-parser middleware

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
 
 /*require all the routes here*/
 const authRouter = require('./routes/auth.routes')
@@ -6,6 +7,7 @@ const authRouter = require('./routes/auth.routes')
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser())
 
 /*using all the routes here */
   app.use('/api/auth', authRouter)
