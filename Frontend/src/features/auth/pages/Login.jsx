73b8eca6eb49e973d8a9c5354d3f13ef3be6a4a2
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-06-21 19:20:37 +0530

    feat: add auth module with login page and form styling

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,7 +1,34 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  import '../auth.form.scss'
 
-export default Login
+  const Login = () => {
+    return (
+      <main>
+        <div className="form-container">
+          <h1>Login</h1>
+          <form >
+            <div className="input-group">
+              <label htmlFor="username">Username</label>
+              <input 
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Enter Username"
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input 
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter Password"
+              />
+            </div>
+            <button className="button primary-button">Submit</button>
+          </form>
+        </div>
+      </main>
+    )
+  }
+
+  export default Login
