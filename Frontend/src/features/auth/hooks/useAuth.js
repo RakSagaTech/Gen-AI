6dc6684d6931a8075adfaab5e78adb883804aa72
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-06-22 21:49:24 +0530

    feat(auth): implement auth actions in useAuth hook

---
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,54 @@
+import {useContext} from 'react'
+import { AuthContext} from '../context/auth.context'
+import {login, register, logout} from '../services/auth.api'
+
+
+
+export const useAuth = () =>{
+  const context =   useContext(AuthContext)
+
+  const {user, setUser, loading, setLoading} = context
+
+
+  const handleLogin = async ({username, password}) =>{
+    setLoading(true)
+    try{
+      const data = await login({username, password})
+      setUser(data)
+    }catch(err){
+      console.error(err)
+      throw err
+    }finally{
+      setLoading(false)
+    }
+  }
+
+  const handleRegister = async ({username, password, email}) =>{
+    setLoading(true)
+    try{
+      const data = await register({username, password, email})
+      setUser(data.user)
+    }catch(err){
+      console.error(err)
+      throw err
+    }finally{
+      setLoading(false)
+    }
+  }
+
+  const handleLogout = async () =>{
+    setLoading(true)
+    try{
+      await logout()
+      setUser(null)
+    }catch(err){
+      console.error(err)
+      throw err
+    }finally{
+      setLoading(false)
+    }
+  }
+  
+
+  return {user, loading, handleLogin, handleRegister, handleLogout}
+}
