270d31fadb0b9b7bd95e0a31ffb2f827acb797f7
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-06-26 21:14:40 +0530

    fix(interview): set initial loading state to false in InterviewProvider

--- Frontend/src/features/interview/context/interview.provider.jsx
+++ Frontend/src/features/interview/context/interview.provider.jsx
@@ -6,7 +6,7 @@ import { InterviewContext } from "./interview.context";
 export const InterviewProvider = ({children}) =>{
   const [report, setReport] = useState(null)
   const [reports, setReports] = useState(null)
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
 
   return (
     <InterviewContext.Provider value={{report, setReport, loading, setLoading, reports, setReports}}>
