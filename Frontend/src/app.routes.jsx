cd36a8c834cbcbbf6224005a3a0ccf4f929aa476
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-06-26 18:20:35 +0530

    feat: create interview report page UI

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -2,6 +2,7 @@ import {createBrowserRouter} from 'react-router'
 import Login from './features/auth/pages/Login'
 import Register from './features/auth/pages/Register'
 import Home from './features/interview/pages/Home'
+import Interview from './features/interview/pages/Interview'
 import Protected from './features/auth/components/Protected'
 
 
@@ -17,5 +18,9 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <Protected><Home /></Protected>
+  },
+  {
+    path: '/interview/:interviewId',
+    element: <Protected><Interview /></Protected>
   }
 ])
